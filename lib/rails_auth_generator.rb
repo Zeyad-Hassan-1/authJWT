a1a7592cdd25ba27d90d64df4b532d269ca925ab
Zeyad Hassan <studying.zezo@gmail.com>
2025-08-18 21:10:43 +0300

    Initialize RailsAuthGenerator gem structure with essential files and configurations

---
+++ lib/rails_auth_generator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rails_auth_generator/version"
+
+module RailsAuthGenerator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rails_auth_generator_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RailsAuthGenerator do
+  it "has a version number" do
+    expect(RailsAuthGenerator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
